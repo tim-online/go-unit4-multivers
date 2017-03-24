13079edb539aa1674d132629374069194ae033ae
Infinityart <jonty_wolf@hotmail.com>
2017-03-24 12:52:55 +0100

    Added AdministrationNVL

--- client.go
+++ client.go
@@ -45,6 +45,7 @@ type Client struct {
 	AdministrationInfoList  *AdministrationInfoListService
 	AdministrationGroup     *AdministrationGroupService
 	AdministrationGroupList *AdministrationGroupListService
+	AdministrationNVL       *AdministrationNVLService
 
 	// Administration data
 	Address                     *AddressService
@@ -92,6 +93,7 @@ func NewClient(httpClient *http.Client, baseURL *url.URL) *Client {
 	c.AdministrationInfoList = NewAdministrationInfoListService(c)
 	c.AdministrationGroup = NewAdministrationGroupService(c)
 	c.AdministrationGroupList = NewAdministrationGroupListService(c)
+	c.AdministrationNVL = NewAdministrationNVLService(c)
 
 	// Administration data
 	c.Address = NewAddressService(c)
